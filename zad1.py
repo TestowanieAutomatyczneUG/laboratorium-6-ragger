4f26491483a6167f1c024efec196d8137c56c8d4
Michał Bidziński <michal.bidzinski1337@gmail.com>
2021-11-14 14:10:07 +0100

    test4GreenNoRefactor

--- zad1.py
+++ zad1.py
@@ -17,7 +17,6 @@ class HammingTest(unittest.TestCase):
     def test_single_letter_different_strands(self):
         self.assertEqual(self.hamming.distance("G", "T"), 1)
 
-    @unittest.skip
     def test_long_identical_strands(self):
         self.assertEqual(self.hamming.distance("GGACTGAAATCTG", "GGACTGAAATCTG"), 0)
 
