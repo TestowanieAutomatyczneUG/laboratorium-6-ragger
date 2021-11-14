9ef45e20eb72029b95561e9c7d70e07157046fcd
Michał Bidziński <michal.bidzinski1337@gmail.com>
2021-11-14 15:14:46 +0100

    test_dissalow_2_green

--- zad1.py
+++ zad1.py
@@ -12,6 +12,8 @@ class Hamming:
             return count
         if len(str1) > len(str2):
             raise ValueError("Pierwszy str dłuższy od drugiego")
+        if len(str2) > len(str1):
+            raise ValueError("Drugi str dluzszy  od pierwszego")
 
 class HammingTest(unittest.TestCase):
     def test_empty_strands(self):
