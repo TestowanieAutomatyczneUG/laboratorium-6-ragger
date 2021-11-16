7eff2cf5f593f82eaa3d6542e49959b66fef4691
Michał Bidziński <michal.bidzinski1337@gmail.com>
2021-11-16 22:06:43 +0100

    get first verse test

--- zad3/testy_zad3.py
+++ zad3/testy_zad3.py
@@ -2,8 +2,12 @@ import unittest
 from zad3 import ChristmasSong
 
 class ChristmasSongTest(unittest.TestCase):
-	def setUp(self):
-		self.christmasSong = ChristmasSong()
+    def setUp(self):
+        self.christmasSong = ChristmasSong()
+
+
+    def test_getLine_0(self):
+        self.assertEqual(self.christmasSong.getLine(0),"On the first day of Christmas my true love gave to me: a Partridge in a Pear Tree.")
 
 if __name__ == "__main__":
-	unittest.main()
+    unittest.main()
