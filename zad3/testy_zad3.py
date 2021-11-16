8f8c82d9bc9e9696bba5fc6835425f1da772ffa9
Michał Bidziński <michal.bidzinski1337@gmail.com>
2021-11-16 22:20:58 +0100

    get verses test

--- zad3/testy_zad3.py
+++ zad3/testy_zad3.py
@@ -6,8 +6,11 @@ class ChristmasSongTest(unittest.TestCase):
         self.christmasSong = ChristmasSong()
 
 
-    def test_getLine_0(self):
-        self.assertEqual(self.christmasSong.getLine(0),"On the first day of Christmas my true love gave to me: a Partridge in a Pear Tree.")
+    def test_getVerse_0(self):
+        self.assertEqual(self.christmasSong.getVerse(0),"On the first day of Christmas my true love gave to me: a Partridge in a Pear Tree.")
+    def test_getVerses_0_2(self):
+        self.assertEqual(self.christmasSong.getVerses(0,2), ["On the first day of Christmas my true love gave to me: a Partridge in a Pear Tree.",
+				"On the second day of Christmas my true love gave to me: two Turtle Doves, and a Partridge in a Pear Tree."])
 
 if __name__ == "__main__":
     unittest.main()
