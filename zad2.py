12cabc746fba7cdc5fdf3be27a41eb4819c49aaa
Michał Bidziński <michal.bidzinski1337@gmail.com>
2021-11-16 20:10:55 +0100

    test11red

--- zad2.py
+++ zad2.py
@@ -49,7 +49,6 @@ class RomanNumeralsTest(unittest.TestCase):
     def test_50_is_a_single_l(self):
         self.assertEqual(roman(59), "LIX")
 
-    @unittest.skip
     def test_90_being_100_10_is_xc(self):
         self.assertEqual(roman(93), "XCIII")
 
