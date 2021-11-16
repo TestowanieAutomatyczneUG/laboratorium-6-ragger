cddaeeb92b7f9ed311364b09c4f559930fccf53a
Michał Bidziński <michal.bidzinski1337@gmail.com>
2021-11-16 22:00:49 +0100

    testy

---
+++ zad3/testy_zad3.py
@@ -0,0 +1,9 @@
+import unittest
+from zad3 import ChristmasSong
+
+class ChristmasSongTest(unittest.TestCase):
+	def setUp(self):
+		self.christmasSong = ChristmasSong()
+
+if __name__ == "__main__":
+	unittest.main()
