b47258bcf139a3dc88971007a7eb1e1de3e57184
Michał Bidziński <michal.bidzinski1337@gmail.com>
2021-11-16 20:08:59 +0100

    test9red

--- zad2.py
+++ zad2.py
@@ -39,7 +39,6 @@ class RomanNumeralsTest(unittest.TestCase):
     def test_20_is_two_x_s(self):
         self.assertEqual(roman(27), "XXVII")
 
-    @unittest.skip
     def test_48_is_not_50_2_but_rather_40_8(self):
         self.assertEqual(roman(48), "XLVIII")
 
