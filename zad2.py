e96f9e3078137ba70251f1073f8413151d83ca9e
Michał Bidziński <michal.bidzinski1337@gmail.com>
2021-11-16 20:12:23 +0100

    test13green

--- zad2.py
+++ zad2.py
@@ -55,7 +55,6 @@ class RomanNumeralsTest(unittest.TestCase):
     def test_100_is_a_single_c(self):
         self.assertEqual(roman(141), "CXLI")
 
-    @unittest.skip
     def test_60_being_50_10_is_lx(self):
         self.assertEqual(roman(163), "CLXIII")
 
